23285d3ce50088a3db9fb60aad05c61e9947deee
RutvikJagtap <114944210+RutvikJagtap@users.noreply.github.com>
2024-03-31 02:06:17 +0530

    Create test.py

---
+++ New folder/test.py
@@ -0,0 +1,13 @@
+from firebase import Firebase
+
+config = {
+  "apiKey": "AIzaSyA0hu3OvW065ZSddZYEmOwMeXSVSk8kADg",
+  "authDomain": "flaskinputoutput.firebaseapp.com",
+  "projectId": "flaskinputoutput",
+  "storageBucket": "flaskinputoutput.appspot.com",
+  "messagingSenderId": "922607453017",
+  "appId": "1:922607453017:web:273364a6c3e5cae76c4248",
+  "measurementId": "G-EF3G0C3SGD"
+}
+
+firebase = Firebase(config)
